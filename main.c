76ee0f41e9339312fd969c720531cba8876d292e
Emil Fresk <emil.fresk@gmail.com>
2014-07-11 20:39:23 +0200

    Added a base-project to test the code on

---
+++ main.c
@@ -0,0 +1,46 @@
+#include "ch.h"
+#include "hal.h"
+#include "myusb.h"
+
+int main(void)
+{
+    /*
+     * System initializations.
+     * - HAL initialization, this also initializes the configured 
+     *   device drivers and performs the board-specific initializations.
+     * - Kernel initialization, the main() function becomes a thread
+     *   and the RTOS is active.
+     */
+    halInit();
+    chSysInit();
+
+    /*
+     *
+     * Initializes a serial-over-USB CDC driver.
+     * 
+     */
+    sduObjectInit(&SDU1);
+    sduStart(&SDU1, &serusbcfg);
+
+    /*
+     *
+     * Activates the USB driver and then the USB bus pull-up on D+.
+     * 
+     */
+    usbDisconnectBus(serusbcfg.usbp);
+    chThdSleepMilliseconds(500);
+    usbStart(serusbcfg.usbp, &usbcfg);
+    usbConnectBus(serusbcfg.usbp);
+
+    /* 
+     *
+     *  User application.
+     *
+     */
+
+    while(1)
+    {
+        palTogglePad(GPIOC, GPIOC_LED_USR);
+        chThdSleepMilliseconds(200);
+    }
+}
