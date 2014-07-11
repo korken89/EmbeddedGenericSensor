17a8d86d50a96cbdf4e3ec066461e1cfa54351d7
Emil Fresk <emil.fresk@gmail.com>
2014-07-11 21:13:37 +0200

    Made a sensor module

--- main.c
+++ main.c
@@ -1,26 +1,7 @@
 #include "ch.h"
 #include "hal.h"
 #include "myusb.h"
-
-struct _sensor
-{
-    msg_t (* init_sensor)(void);
-    msg_t (* read_sensor)(void);
-};
-
-typedef struct
-{
-    uint16_t frequency_hz;
-    struct _sensor sensor;
-    virtual_timer_t *vt;
-} polled_sensor_t;
-
-typedef struct
-{
-    expchannel_t interrupt_channel;
-    uint32_t mode;
-    struct _sensor sensor;
-} interrupt_sensor_t;
+#include "sensors.h"
 
 msg_t my_polled_sensor1_init(void);
 msg_t my_polled_sensor1_read(void);
@@ -32,14 +13,14 @@ msg_t my_polled_sensor2_read(void);
 static virtual_timer_t vt1;
 static virtual_timer_t vt2;
 
-const polled_sensor_t polled_sensors[] = {
+static const polled_sensor_t polled_sensors[] = {
     {
         .frequency_hz = 80,
         .sensor = {
             .init_sensor = my_polled_sensor1_init,
             .read_sensor = my_polled_sensor1_read
         },
-        .vt = &vt1
+        .polling_vt = &vt1
     },
     {
         .frequency_hz = 10,
@@ -47,7 +28,7 @@ const polled_sensor_t polled_sensors[] = {
             .init_sensor = my_polled_sensor2_init,
             .read_sensor = my_polled_sensor2_read
         },
-        .vt = &vt2
+        .polling_vt = &vt2
     }
 };
 
